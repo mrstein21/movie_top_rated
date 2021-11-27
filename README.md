6457e27d61f43fd4ac9ffd5fa0441238bff4d927
rangga <mohammadrangga27@gmail.com>
2021-11-27 11:13:37 +0700

    initial

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
-# news_trial
+# movie_app
 
-A new Flutter application.
+Flutter App using HTTP library to call TheMovieDB API and Load More feature with Notification Listener
 
-## Getting Started
+## Screen Shoot
+ ![](images/ss1.jpeg)
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/home.dart
+++ lib/home.dart
@@ -4,117 +4,102 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class Home extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _HomeState extends State<Home> {
-  List<dynamic>data=[];
-  bool isEnough=false;
-  int page=1;
-  bool isLoading=true;
+class _HomePageState extends State<HomePage> {
+  List<dynamic>list_movies=[];
+  String image="https://image.tmdb.org/t/p/w500";
 
-  void getData(){
-     isLoading=true;
-     http.get(Uri.parse("https://api.themoviedb.org/3/movie/top_rated?api_key=b42f1d2342c381ec25f6180c52e51c00&page=$page"))
-         .then((http.Response value){
-       isEnough=true;
-       isLoading=false;
-       Map<String,dynamic> response=json.decode(value.body);
-       print(response);
-       data=[...data,...response["results"]];
+  void callAPI2()async{
+    try{
+      http.Response value=await http.get(Uri.parse("https://api.themoviedb.org/3/movie/top_rated?api_key=b42f1d2342c381ec25f6180c52e51c00"));
+      if(value.statusCode==200){
+        Map<String,dynamic> json_decode=json.decode(value.body);
+        list_movies=json_decode["results"];
+        setState(() {
 
-       setState(() {
+        });
+      }else{
+        print("Something Wrong");
+      }
+    }catch(error,track){
+      print("error "+error.toString());
+      print("error at "+track.toString());
+    }
+  }
 
-       });
-     }).onError((error, stackTrace){
-       print("terjadi kesalahan "+error.toString());
-     });
+  void callAPI(){
+    http.get(Uri.parse("https://api.themoviedb.org/3/movie/top_rated?api_key=b42f1d2342c381ec25f6180c52e51c00"))
+    .then(( http.Response value){
+      if(value.statusCode==200){
+        Map<String,dynamic> json_decode=json.decode(value.body);
+        list_movies=json_decode["results"];
+        setState(() {
+
+        });
+      }else{
+        print("Terjadi kesalahan");
+      }
+    }).onError((error, stackTrace){
+
+    });
   }
 
   @override
   void initState() {
-    getData();
+    callAPI2();
     // TODO: implement initState
     super.initState();
   }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Top Rated Movies"),
+        title: Text("Top Rated Movies",),
       ),
       body: Container(
-        padding: EdgeInsets.all(10),
-        child: data.isNotEmpty?_buildList():_buildCircular()
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+            itemCount: list_movies.length,
+            itemBuilder: (BuildContext context,int index){
+              return Container(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 6),
+                      child: Image.network(image+list_movies[index]["poster_path"],
+                          width: 80,height: 120,),
+                    ),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(list_movies[index]["title"],style:
+                            TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
+                          SizedBox(height: 5,),
+                          Text("Release on "+list_movies[index]["release_date"]),
+                          SizedBox(height: 5,),
+                          Text(list_movies[index]["overview"],
+                              maxLines:4,overflow: TextOverflow.ellipsis,)
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+            }
+        ),
       ),
     );
-  }
-
-  Widget _buildList(){
-    return NotificationListener<ScrollNotification>(
-      child: ListView.builder(
-          itemCount: isEnough==true?data.length:data.length+1 ,
-          itemBuilder: (BuildContext context,int index){
-            if(index<data.length){
-              return _buildRow(data[index]);
-            }else{
-              return _buildCircular();
-            }
-          }
-      ),
-      onNotification: (ScrollNotification scrollInfo){
-        if(scrollInfo.metrics.pixels==scrollInfo.metrics.maxScrollExtent){
-          if(isEnough==false){
-            print("hasil is enough "+isEnough.toString());
-            if(isLoading==false){
-              page=page+1;
-              getData();
-              return true;
-            }
-          }
-        }
-        return false;
-      },
-    );
-  }
-
-  Widget _buildCircular(){
-    return Center(
-      child: CircularProgressIndicator(),
-    );
-  }
-  
-  Widget _buildRow(Map<String,dynamic> data){
-     return Container(
-       margin: EdgeInsets.all(3),
-       child: Row(
-         mainAxisAlignment: MainAxisAlignment.start,
-         crossAxisAlignment: CrossAxisAlignment.start,
-         children: [
-           Image.network("https://image.tmdb.org/t/p/w500"+data["poster_path"],
-             height: 120,width: 80,fit: BoxFit.fill,),
-           SizedBox(width: 6,),
-           Expanded(
-             child: Column(
-               mainAxisAlignment: MainAxisAlignment.start,
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [
-                 Text(data["title"],style: TextStyle(
-                     fontSize: 15,
-                     fontWeight: FontWeight.bold
-                 ),maxLines: 2,overflow: TextOverflow.ellipsis),
-                 SizedBox(height: 4,),
-                 Text("Release on : "+data["release_date"],),
-                 SizedBox(height: 4,),
-                 Text(data["overview"],style: TextStyle(),maxLines: 4,overflow: TextOverflow.ellipsis,)
-               ],
-             ),
-           ),
-         ],
-       ),
-     );
   }
 }
+
